30db3b2b0bb81c652b61b26ea3fadeb1c3346bd7
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-12-16 15:06:08 -0200

    setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get install -y ruby-full
 apt-get update && apt-get install -y git
-gem install bundler
-gem install rails
+gem install rake && bundle install
 git clone https://github.com/ricardobaumann/redmine
 cd redmine
 bundle install
