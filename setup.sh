ffbca8dc0019f4206ca44699a5675848f67b2378
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-12-16 15:20:20 -0200

    setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ apt-get update && apt-get install -y ruby-full
 apt-get update && apt-get install -y git
 gem install rake
 gem install bundler
+gem install nokogiri -v '1.6.5'
 git clone https://github.com/ricardobaumann/redmine
 cd redmine
 bundle install
