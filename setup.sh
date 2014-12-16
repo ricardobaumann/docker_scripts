6a5bf4f4d441ceebc7fcb33180123a6a39b6bb90
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-12-16 16:14:14 -0200

    setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt-get update && apt-get install -y ruby-full
-apt-get update && apt-get install -y git
+apt-get install -y git
+apt-get install -y libxml2
+apt-get install -y libxslt
 gem install rake
 gem install bundler
 gem install nokogiri -- --use-system-libraries
