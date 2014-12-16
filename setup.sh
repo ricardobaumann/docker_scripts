df4c124b6f2b808f2b8c4503bff19f68be0aa3f5
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-12-16 16:22:09 -0200

    setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-apt-get update && apt-get install -y ruby-full
 apt-get install -y git
 apt-get install -y libxml2
 apt-get install -y libxslt
+apt-get install curl
+curl -sSL https://get.rvm.io | bash -s stable --rails
+rvm install 1.9.3
+rvm use 1.9.3
 gem install rake
 gem install bundler
-gem install nokogiri -- --use-system-libraries
+gem install nokogiri -v '1.6.5'
 git clone https://github.com/ricardobaumann/redmine
 cd redmine
 bundle install
