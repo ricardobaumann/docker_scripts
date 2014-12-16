8f4182ea06194f00bf3cc4f3008054f3b3c08394
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-12-16 16:28:17 -0200

    setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ apt-get install -y git
 apt-get install -y libxml2
 apt-get install -y libxslt
 apt-get install -y curl
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --rails
 rvm install 1.9.3
 rvm use 1.9.3
