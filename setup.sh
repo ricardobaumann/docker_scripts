71cbe88cbef0cdc4ef9f7146209f824ae4173db0
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-12-17 13:19:55 -0200

    testes

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ apt-get install -y curl
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --rails
 source /usr/local/rvm/scripts/rvm
-rvm install 1.9.3
-rvm use 1.9.3
+#rvm install 1.9.3
+#rvm use 1.9.3
 gem install rake
 gem install bundler
-gem install nokogiri -v '1.6.5'
-gem install rails -v '3.2.19'
+#gem install nokogiri -v '1.6.5'
+#gem install rails -v '3.2.19'
 git clone https://github.com/ricardobaumann/inventory_on_rails
 cd inventory_on_rails
 bundle install --path vendor/cache
