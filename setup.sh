b75cb1154f498fce93f24f749d8fbfee5d4c5318
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2014-12-17 19:48:38 -0200

    testes

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ gem install rake
 gem install bundler
 gem install rails
 
-
 #gem install nokogiri -v '1.6.5'
 #gem install rails -v '3.2.19'
 git clone https://github.com/ricardobaumann/inventory_on_rails
